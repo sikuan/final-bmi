fe0314b4809cddd0a3d94f9d15e9b8bde4a93d1e
Sao I Kuan <saoikuan@gmail.com>
2022-12-13 01:10:25 -0500

    Initial Commit

---
+++ final-bmi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final-bmi
+//
+//  Created by Cadence Kuan on 13/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
